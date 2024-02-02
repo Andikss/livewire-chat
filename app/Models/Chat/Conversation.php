d1d9bda35ab7175639f4a71071a69caa4256b1be
JazzBego <andika.dwi.saputra.7925@gmail.com>
2024-02-02 20:38:10 +0700

    update: defined default value for uuid

--- app/Models/Chat/Conversation.php
+++ app/Models/Chat/Conversation.php
@@ -14,6 +14,7 @@ class Conversation extends Model
     protected $table      = 'conversation';
 
     protected $fillable   = [
+        'id',
         'receiver_id',
         'sender_id'
     ];
--- app/Models/Chat/Message.php
+++ app/Models/Chat/Message.php
@@ -14,6 +14,7 @@ class Message extends Model
     protected $table      = 'messages';
 
     protected $fillable   = [
+        'id',
         'body',
         'sender_id',
         'receiver_id',
